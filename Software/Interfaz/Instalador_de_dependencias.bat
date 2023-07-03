bf9162ed50c654e4395ca3f6baccee9e03bcd85b
KatherineParra2160463 <134155081+KatherineParra2160463@users.noreply.github.com>
2023-07-03 13:18:53 -0500

    INSTALADOR DE DEPENDENCIAS
    Añado el instalador de dependencias, el cual instalará todo lo necesario para el correcto funcionamiento de la interfaz.

---
+++ Software/Interfaz/Instalador_de_dependencias.bat
@@ -0,0 +1,30 @@
+@echo off
+
+echo Instalando numpy...
+pip install numpy==1.24.1
+
+echo Instalando matplotlib...
+pip install matplotlib==3.6.3
+
+echo Instalando pandas...
+pip install pandas==1.5.3
+
+echo Instalando OpenCV (cv2)...
+pip install opencv-python==4.7.0
+
+echo Instalando openpyxl...
+pip install openpyxl==3.1.2
+
+echo Instalando PyQt5...
+pip install PyQt5==5.15.9
+
+echo Instalando Ultralytics(YOLO)
+pip install ultralytics==8.0.51
+
+echo Instalando Deskew
+pip install deskew==1.4.3
+
+echo Instalando Typing-extensions
+pip install typing-extensions==4.4.0
+
+echo Todas las dependencias han sido instaladas.
